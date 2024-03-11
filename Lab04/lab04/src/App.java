b2f7695323ff892107717306a8daccadd62efda0
Tomasz Nowak <76777800+Tnovyloo@users.noreply.github.com>
2024-03-11 14:34:28 +0100

    Finishing all exercises for lab04

--- Lab04/lab04/src/App.java
+++ Lab04/lab04/src/App.java
@@ -1,10 +1,16 @@
+import java.util.Objects;
 import java.util.Random;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
         // exercise1();
-        exercise2();
+        // exercise2();
+        // exercise3();
+        // exercise4();
+        // exercise5();
+        // exercise6();
+        exercise7();
     }
 
     // Help functions
@@ -26,6 +32,15 @@ public class App {
     public static int randomNumber(int a, int b) {
         Random randint = new Random();
         return randint.nextInt(a, b);
+    }
+
+    // Factorial help function for exercise 6
+    public static int factorial(int a) {
+        int temp = 1;
+        for (int i = 1; i <= a; i++) {
+            temp *= i;
+        }
+        return temp;
     }
 
     // Function for exercise1
@@ -84,6 +99,121 @@ public class App {
     // Function for exercise 3
     public static void exercise3() {
         String[] stringArray = {"Fucha się trafiła, to bierzemy. Co To za Sztuka Skrecic Dwa Kabelki", "Bogdan, tu są trzy fazy.", "No to trzy kabelki, wielka mi różnica..."};
+
+        for (String string : stringArray) {
+            System.out.println(string.toUpperCase());
+        }
+    }
+
+    // Function for exercise 4
+    public static void exercise4() {
+        // Fill string with user inputs.
+        String[] stringArray = new String[5];
+
+        for (int i = 0; i < stringArray.length; i++) {
+            stringArray[i] = inputString("tekst");
+        }
+
+        // Reverse this array.
+        int lastIndex = stringArray.length - 1;
+
+        for (int i = 0; i < stringArray.length / 2; i++) {
+            String tempString = stringArray[i];
+            stringArray[i] = stringArray[lastIndex];
+            stringArray[lastIndex] = tempString;
+            lastIndex--;
+        }
+
+        // Printing reversed array.
+        System.out.println("Tablica po odwroceniu.");
+        for (int i = 0; i < stringArray.length; i++) {
+            System.out.println(stringArray[i]);
+        }
+        System.out.println("\n");
+
+        // Print reversed word in reversed array.
+        for (int i = 0; i < stringArray.length; i++) {
+            for (int j = stringArray[i].length() - 1; j >= 0; j--) {
+                System.out.print(stringArray[i].charAt(j));
+            }
+            System.out.println("");
+        }
+
+    }
+
+    public static void exercise5() {
+        System.out.println("Sortowanie tablicy int.");
+
+        int[] intArray = new int[8];
+        // int[] intArray = {5, 3, 2, 6, 1, 9, 10};
+
+        // Input values
+        for (int i = 0; i < intArray.length; i++) {
+            String inputText = "Tablica[" + i + "]";
+            intArray[i] = inputInteger(inputText);
+        }
+
+        System.out.println("");
+
+        // Sort algorithm
+        // In nested for loop (second one) i start over int j = i and not from j = 0 so the result of this algorhitm was wrong ;P
+        for (int i = 0; i < intArray.length; i++) {
+            for (int j = 0; j < intArray.length - 1; j++) {
+                if (intArray[j] > intArray[j + 1]) {
+                    int temp = intArray[j + 1];
+                    intArray[j + 1] = intArray[j];
+                    intArray[j] = temp;
+                }
+            }
+        }
+
+        // Print values in array.
+        for (int i : intArray) {
+            System.out.println(i);
+        }
+    }
+
+    public static void exercise6() {
+        int[] intArray = new int[8];
         
+        for (int i = 0; i < intArray.length; i++) {
+            intArray[i] = inputInteger("Wartosc");
+        }
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(factorial(intArray[i]));
+        }
+    }
+
+    public static void exercise7() {
+        // Input length of arrays and values.
+        int lengthOfArrays = inputInteger("wielkosc porownywanych tablicy");
+
+        String[] stringArray = new String[lengthOfArrays];
+        String[] stringArray2 = new String[lengthOfArrays];
+
+        System.out.println("Podaj wartosci dla tablicy 1");
+        for (int i = 0; i < stringArray.length; i++) {
+            stringArray[i] = inputString("slowo");
+        }
+
+        System.out.println("Podaj wartosci dla tablicy 2");
+        for (int i = 0; i < stringArray2.length; i++) {
+            stringArray2[i] = inputString("slowo");
+        }
+
+        // Check if arrays are the same.
+        System.out.println("Porownuje tablice");
+        
+        boolean isTheSame = true;
+
+        for (int i = 0; i < stringArray.length; i++) {
+            if (!(Objects.equals(stringArray[i], stringArray2[i]))) {
+                isTheSame = false;
+                break;
+            }
+        }
+
+        System.out.println("Czy sa takie same? " + isTheSame);
     }
 }
