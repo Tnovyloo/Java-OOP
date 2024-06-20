c605a898fc689d391f1ed85f5bfe183a14b798c8
Tomasz Nowak <76777800+Tnovyloo@users.noreply.github.com>
2024-06-20 23:37:40 +0200

    Adding new folder with algorithms

--- Lab12/lab12/src/main/java/lab12/PrimaryController.java
+++ Lab12/lab12/src/main/java/lab12/PrimaryController.java
@@ -1,6 +1,10 @@
 package lab12;
 
 import java.io.IOException;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+
 import javafx.fxml.FXML;
 
 public class PrimaryController {
@@ -13,5 +17,24 @@ public class PrimaryController {
     @FXML
     private void printToConsole() throws IOException {
         System.out.println("TRYING TO DO SMTH");
+    }
+
+    @FXML
+    private void showClosePanel() throws IOException {
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle("Zamknąć program?");
+        alert.setHeaderText("Zamknąć program?");
+
+        ButtonType yesButtonType = new ButtonType("Tak");
+        ButtonType noButtonType = new ButtonType("Nie");
+        alert.getButtonTypes().setAll(yesButtonType, noButtonType);
+
+        alert.showAndWait().ifPresent(response -> {
+            if (response == yesButtonType) {
+                System.out.println("Yesesss button!");
+            } else {
+                System.out.println("Noooo button!");
+            }
+        });
     }
 }
