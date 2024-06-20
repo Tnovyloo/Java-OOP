21df531b1538ac03d1d054770650b04726e9ea2e
Tomasz Nowak <76777800+Tnovyloo@users.noreply.github.com>
2024-06-21 00:27:26 +0200

    Creating prime number generator

--- Algorithms1/Algorithms/src/App.java
+++ Algorithms1/Algorithms/src/App.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        NWD(13, 169);
+        // NWD(13, 169);
+        primeNumber(100);
     }
 
 
@@ -15,5 +19,27 @@ public class App {
         }
 
         System.out.println(a);
+    }
+
+    public static void primeNumber(int a) {
+        if (a == 2) {
+            System.out.println(a);
+        }
+        for (int i = 3; i < a; i++) {
+            // Search for number dividors.
+            List<Integer> dividorsArrayLists = new ArrayList<Integer>();
+
+            for (int j = 1; j <= Math.sqrt(i); j++) {
+            // for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    dividorsArrayLists.add(j);
+                }
+            }
+
+            // System.out.println(i + " dividors: " + dividorsArrayLists);
+            if (dividorsArrayLists.size() == 1) {
+                System.out.println("Prime number: " + i);
+            }
+        }
     }
 }
