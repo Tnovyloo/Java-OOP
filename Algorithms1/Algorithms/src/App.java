e9d80741e7f1341adbf3e9cb05fc3961ac39906e
Tomasz Nowak <76777800+Tnovyloo@users.noreply.github.com>
2024-06-23 16:04:20 +0200

    Creating CubeSort

--- Algorithms1/Algorithms/src/App.java
+++ Algorithms1/Algorithms/src/App.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -13,7 +16,10 @@ public class App {
         // primeFactorization(12);
         // primeFactorization(1);
         // primeFactorization(44100);
-        LCG(12, 13, 7, 0);
+        // LCG(12, 13, 7, 0);
+        // System.out.println(fibonacciSequence(10));
+        
+        listTestFunction();
     }
 
 
@@ -85,13 +91,102 @@ public class App {
 
     public static void LCG(int mod, int multipler, int gain, int seed) {
         // pseudorandom number linear congruential generator
+        List<Integer> randomNumbers = new ArrayList<Integer>();
+
         int x = seed;
         int lastResult = 0;
         do {
             x = (multipler * (lastResult + gain)) % mod;
             lastResult = x;
-            System.out.println(x);
+            randomNumbers.add(x);
         } while (x != seed);
+
+        System.out.println(randomNumbers);
+    }
+
+    public static int fibonacciSequence(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacciSequence(n-1) + fibonacciSequence(n-2);
+        }
+    }
+
+
+    public static void listTestFunction() {
+        // function for fun with arraylists
+
+        List<Integer> arrayList = new ArrayList<>(Arrays.asList(5, 3, 5, 3, 9, 8, 11, 45, 21, 42, 1, 3, 34, 0, 1, 423, 501));
+        
+        // Reverse arrayList 
+        List<Integer> arrayListCopy = new ArrayList<>();
+
+        for (int i = arrayList.size() -1; i >= 0; i--) {
+            arrayListCopy.add(arrayList.get(i));
+        }
+        System.out.println("Original list:" + arrayList + "\nReversed list: " + arrayListCopy);
+
+        // Sorting list
+        for (int i = 0; i < arrayListCopy.size() - 1; i++) {
+            for (int j = i + 1; j < arrayListCopy.size(); j++) {
+                int iNumber = arrayListCopy.get(i);
+                int jNumber = arrayListCopy.get(j);
+                if (iNumber > jNumber) {
+                    arrayListCopy.set(j, iNumber);
+                    arrayListCopy.set(i, jNumber);
+                }
+            }
+        }
+
+        System.out.println("Sorted arraylist by insert sorting alghorithm " + arrayListCopy);
+
+        // Numbers counter
+        // HashMap<Integer, Integer> bucketHashMap = new HashMap<>();
+
+        // Counter for same integers in ArrayList.
+        // for (int i = 0; i < arrayList.size(); i++) {
+        //     if (bucketHashMap.get(arrayList.get(i)) == null) {
+        //         bucketHashMap.put(arrayList.get(i), 1);
+        //     } else {
+        //         int value = bucketHashMap.get(arrayList.get(i));
+        //         bucketHashMap.put(arrayList.get(i), value + 1);
+        //     }
+        // }
+        // System.out.println(bucketHashMap);
+
+
+        // Cubesort
+
+        // Create buckets from 0 to max(arraylist)
+        HashMap<Integer, Integer> bucketMaxHashMap = new HashMap<>();
+        System.out.println(arrayList);
+
+        int maxNumber = arrayList.get(0);
+        for (int i = 1; i < arrayList.size(); i++) {
+            if (arrayList.get(i) > maxNumber) {
+                maxNumber = arrayList.get(i);
+            }
+        }
+
+        for (int i = 0; i <= maxNumber; i++) {
+            bucketMaxHashMap.put(i, 0);
+        }
+
+        // for (int i = 0; i < arrayList.size(); i++) {
+        //     bucketMaxHashMap.put(arrayList.get(i), bucketMaxHashMap.get(i) + 1);
+        // }
+
+        for (int i = 0; i < arrayList.size(); i++) {
+            int value = bucketMaxHashMap.get(arrayList.get(i));
+            bucketMaxHashMap.put(arrayList.get(i), value + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : bucketMaxHashMap.entrySet()) {
+            if (entry.getValue() > 0) {
+                System.out.println(entry);
+            }
+        }
+        // System.out.println(bucketMaxHashMap);
 
     }
 }
